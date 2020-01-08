6e419579dadb9dff26a1fa17bfb845922c80cf13
Rafael Rodriguez <rafaelrodriguez@Rafaels-MacBook-Pro.local>
2020-01-07 17:19:52 -0700

    easy stuff

---
+++ index.js
@@ -0,0 +1,30 @@
+let name = 'Mosh';
+console.log(name);
+
+const intrestRate = 0.3;
+console.log(intrestRate);
+
+let person = {
+	name: 'Rafa',
+	age: '24'
+};
+
+person.name = 'the guy';
+
+console.log(person.name);
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = '1';
+console.log(selectedColors);
+
+// Easy Task
+function greet(name, lastName) {
+	console.log('Hello ' + name + '' + lastName);
+}
+
+//calc value
+function square(number) {
+	return number * number;
+}
+
+console.log(square(3));
